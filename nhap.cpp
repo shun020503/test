20c9fc8ddae06157676539f987120135d3683c5a
unknown <kuuhakusorq@gmail.com>
2023-09-20 16:39:06 +0700

    Add nhap.cpp file

---
+++ nhap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+const long long oo = 1e18;
+long long dp[1000001], a[1000001], n;
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) dp[i] = oo;
+
+    dp[0] = dp[1] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i + 1 <= n) dp[i + 1] = min(dp[i + 1], dp[i] + a[i + 1]);
+        if (i + 2 <= n) dp[i + 2] = min(dp[i + 2], dp[i] + a[i + 2]);
+    }
+    
+    cout << dp[n];
+    return 0;
+}
